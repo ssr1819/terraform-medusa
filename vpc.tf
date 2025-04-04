1eb9e9d0cb0ccd4dfafcd664bd395a866ca3d78d
ssr1819 <149999089+ssr1819@users.noreply.github.com>
2025-04-04 16:04:52 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_vpc" "medusa_vpc" {
@@ -12,7 +12,7 @@ resource "aws_subnet" "medusa_subnet" {
   vpc_id                  = aws_vpc.medusa_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "ap-south-1"
 }
 
 resource "aws_security_group" "medusa_sg" {
